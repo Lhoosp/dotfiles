b9c403b1bd6c8d187dd1ea9a51b8dc90b136d871
Lhoos <lm@lm.local>
2023-02-25 01:32:10 +0300

    Add some files

--- other_cli_cmd.sh
+++ other_cli_cmd.sh
@@ -1,7 +1,7 @@
 #!bin/bash
 
 # tpm install
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm #in
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm 
 
 # change shell
-echo $(which fish) | sudo tee -a /etc/shells && sudo chsh -s $(which fish)
+echo $(which fish) | sudo tee -a /etc/shells && chsh -s $(which fish)
