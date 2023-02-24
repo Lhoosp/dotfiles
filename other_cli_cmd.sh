35410fbb76b1fc78180fff6ffa4fc1d019e4bb10
Lhoos <lm@lm.local>
2023-02-25 02:14:06 +0300

    Fix

--- other_cli_cmd.sh
+++ other_cli_cmd.sh
@@ -1,7 +1,7 @@
 #!bin/bash
 
 # tpm install
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm 
+git clone https://github.com/tmux-plugins/tpm ~/.config/tmux/plugins/tpm 
 
 # change shell
 echo $(which fish) | sudo tee -a /etc/shells && chsh -s $(which fish)
