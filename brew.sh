21215177e9ee97b129828cd6fea75a0557993d2d
Lhoosp <125579491+Lhoosp@users.noreply.github.com>
2023-02-25 00:32:06 +0300

    Create brew.sh

---
+++ brew.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+brew install git
+brew install neovim
+brew install tmux
+brew install exa
+brew install --cask figma
+brew install --cask amethyst
+brew install --cask agenda
+brew install --cask iterm2
+brew install fish
+brew install starship
+brew install rustup
+brew tap homebrew/cask-fonts && brew install --cask font-meslo-lg-nerd-font
