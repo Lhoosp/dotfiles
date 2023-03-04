79c0df37af5b3fb5e227f059c0782b3497165038
Lhoos <lm@lm.local>
2023-03-04 05:53:57 +0300

    changed tmux.conf, added init.lua conf

---
+++ .config/nvim/init.lua
@@ -0,0 +1,23 @@
+vim.opt.background = 'light'
+vim.cmd[[colorscheme shine]]
+vim.opt.termguicolors = true
+
+vim.opt.backspace = 'indent,eol,nostop'
+
+vim.opt.breakindent = true
+vim.opt.wrap = false
+vim.opt.autoindent = true
+vim.opt.smartindent = true
+
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+vim.opt.title = true
+
+vim.opt.autoread = true
