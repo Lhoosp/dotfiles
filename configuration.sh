dc64c3e80245902a640fcee2b2e6d20f813c4b04
Lhoos <ll@lm.local>
2023-03-06 06:01:58 +0300

    edit

--- configuration.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-installpath=$HOME/dotfiles/install_scripts
-
-source $installpath/get_new_ssh.sh
-source $installpath/brew.sh
-source $installpath/get_config.sh
-source $installpath/rust.sh
-source $installpath/change_shell.sh
---
+++ scripts/install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+installpath=$HOME/dotfiles/scripts
+
+source $installpath/getSSH.sh
+source $installpath/brew.sh
+source $installpath/copyConfig.sh
+source $installpath/rust.sh
+source $installpath/changeShell.sh
